579fb16d294595867e60b8caf95e7684f6eed9f4
xiangyisss <[xiangyisunn@gmail.com]>
2022-05-13 11:08:30 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Child } from './Components/ChildComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 style={{ color: 'blue' }}>Hello world</h1>
+      <Child name={'Xiang'} age={20} grade={3} />
     </div>
   );
 }
---
+++ src/Components/ChildComponent.tsx
@@ -0,0 +1,23 @@
+import React, { Children } from 'react';
+
+interface PersonalInfo {
+  name: string;
+  middleName?: string;
+  grade: number;
+  age: number;
+  className?: string;
+  style?: object;
+}
+export const Child = ({ name, ...props }: PersonalInfo) => {
+  console.log('This is props', props);
+  const { middleName = 'Happy', grade, age, style, className } = props;
+  return (
+    <div className={className}>
+      <p>This is child component</p>
+      <p style={style}>
+        Hello, My name is {name}, middle name is {middleName}, {age} years old,
+        Now I'm in grade {grade} .
+      </p>
+    </div>
+  );
+};
